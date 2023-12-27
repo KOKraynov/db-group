d8348403fe690209aa79ce8fe942cf44d868cc09
KOKraynov <k.o.kraynov@gmail.com>
2023-12-27 09:58:18 +0400

    Добавление колонки

--- v_experiment_data.sql
+++ v_experiment_data.sql
@@ -1,7 +1,7 @@
 CREATE VIEW `v_experiment_data` AS
   SELECT ed.id, ed.experiment_id, ed.wave_id, e.potato_id, 
          e.description as exp_description,
-         p.description, p.color, w.lyambda
+         p.description, p.color, w.lyambda, ed.measurement
     FROM exp_data ed, potatos p, experiments e, waves w
     WHERE ed.wave_id = w.wave_id
       AND ed.experiment_id = e.experiment_id
